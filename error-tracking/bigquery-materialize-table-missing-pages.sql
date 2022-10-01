9b20fceea6af10fad2e96496b0914e1fc557ffd3
Tony McCreath <tony@websiteadvantage.com.au>
2022-10-01 12:07:06 +0930

    moved from user attribution to page view attribution

--- error-tracking/bigquery-materialize-table-missing-pages.sql
+++ error-tracking/bigquery-materialize-table-missing-pages.sql
@@ -1,4 +1,4 @@
-# Missing Page Materialised Table v3.0
+# Missing Page Materialised Table v3.1
 # https://github.com/Tiggerito/GA4-Scripts/blob/main/error-tracking/bigquery-materialize-table-missing-pages.sql
 
 # Replace all occurances of DatasetID with your Dataset ID
@@ -9,9 +9,12 @@ AS
 SELECT 
     TIMESTAMP_MICROS(event_timestamp) AS event_timestamp,
     DATE_TRUNC(TIMESTAMP_MICROS(event_timestamp), DAY) AS event_date,
-    traffic_source.medium AS traffic_medium,
-    traffic_source.name AS traffic_name,
-    traffic_source.source AS traffic_source,
+    # traffic_source.medium AS traffic_medium, # user level
+    # traffic_source.name AS traffic_name, # user level
+    # traffic_source.source AS traffic_source, # user level
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'source') AS source,
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'medium') AS medium,
+    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'campaign') AS campaign,
     (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location') AS page_location,
     (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_type') AS page_type,
     (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_referrer') AS page_referrer,
