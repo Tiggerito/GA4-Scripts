1cc3c585c4a1c7215d144f3060f79613c41f7271
Tony McCreath <tony@websiteadvantage.com.au>
2022-12-06 15:17:12 +1030

    User Sessions table

--- bigquery-tag-rocket-report-data-incremental2.sql
+++ bigquery-tag-rocket-report-data-incremental2.sql
@@ -700,6 +700,200 @@ BEGIN
   FROM `${ProjectID}.${DatasetID}.events_*` 
   WHERE event_name = 'page_view'
   AND (datetogather IS NULL OR _table_suffix BETWEEN FORMAT_DATE('%Y%m%d',datetogather) AND FORMAT_DATE('%Y%m%d',CURRENT_DATE()));
+
+  # User Sessions
+
+  # If schema is changed then update both references to the version so the table gets re-built
+  IF NOT EXISTS(
+    SELECT
+      1
+    FROM
+      `${ProjectID}.tag_rocket.INFORMATION_SCHEMA.TABLE_OPTIONS`
+    WHERE
+      table_name = 'user_sessions'
+      AND option_name = 'description'
+      AND option_value LIKE "%Version 4.3%" # queryVersion
+  ) 
+  THEN
+    CREATE OR REPLACE TABLE `${ProjectID}.tag_rocket.user_sessions` (
+      last_updated TIMESTAMP,
+      user_pseudo_id STRING,
+      ga_session_id INT64,
+      session_date STRING,
+      session_start_timestamp TIMESTAMP,
+      session_end_timestamp TIMESTAMP,
+      user_id STRING,
+      session_first_visit INT64,
+      session_page_view_count INT64,
+      session_view_item_list_count INT64,
+      session_view_item_count INT64,
+      session_select_item_count INT64,
+      session_add_to_cart_count INT64,
+      session_view_cart_count INT64,
+      session_begin_checkout_count INT64,
+      session_add_customer_info_count INT64,
+      session_add_shipping_info_count INT64,
+      session_add_billing_info_count INT64,
+      session_purchase_count INT64,
+
+      session_purchase_revenue FLOAT64,
+      session_purchase_currency STRING,
+
+      session_device_category	STRING,
+      session_device_browser	STRING,
+      session_device_operating_system	STRING,
+      session_country	STRING,
+      session_landing_page	STRING,
+      session_referrer	STRING,
+      session_campaign	STRING,
+      session_source	STRING,
+      session_medium	STRING,
+      session_term	STRING,
+      session_gclid	STRING,
+      customer_group_name	STRING,
+
+      user_ltv_revenue	FLOAT64,
+      user_ltv_currency	STRING,
+      user_campaign	STRING,
+      user_medium	STRING,
+      user_source	STRING,
+      user_referrer	STRING,
+      user_landing_page	STRING,
+      user_landing_page_type	STRING,
+      user_first_timestamp	TIMESTAMP
+    )
+    PARTITION BY TIMESTAMP_TRUNC(session_start_timestamp, DAY)
+    OPTIONS (description = 'Version 4.3'); # queryVersion
+  END IF;
+
+  ALTER TABLE `${ProjectID}.tag_rocket.user_sessions`
+  SET OPTIONS (partition_expiration_days = 65); # ExpirationDays
+
+  SET datetogather = (SELECT TIMESTAMP_TRUNC(TIMESTAMP_ADD(MAX(PARSE_TIMESTAMP("%Y%m%d",session_date)), INTERVAL -lookbackDays DAY), DAY) FROM `${ProjectID}.tag_rocket.user_sessions`);
+
+  IF datetogather IS NOT NULL THEN
+    DELETE FROM `${ProjectID}.tag_rocket.user_sessions` WHERE PARSE_TIMESTAMP("%Y%m%d",session_date) >= datetogather;
+  ELSE
+    IF maxDaysToLookBackOnInitialQuery IS NOT NULL THEN
+      SET datetogather = TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL maxDaysToLookBackOnInitialQuery DAY);
+    END IF;
+  END IF;
+
+  INSERT `${ProjectID}.tag_rocket.user_sessions` 
+  (  
+      user_pseudo_id,
+      ga_session_id,
+      last_updated,
+      session_date,
+      session_start_timestamp,
+      session_end_timestamp,
+      user_id,
+      session_first_visit,
+      session_page_view_count,
+      session_view_item_list_count,
+      session_view_item_count,
+      session_select_item_count,
+      session_add_to_cart_count,
+      session_view_cart_count,
+      session_begin_checkout_count,
+      session_add_customer_info_count,
+      session_add_shipping_info_count,
+      session_add_billing_info_count,
+      session_purchase_count,
+
+      session_purchase_revenue,
+      session_purchase_currency,
+
+      session_device_category,
+      session_device_browser,
+      session_device_operating_system,
+      session_country,
+      session_landing_page,
+      session_referrer,
+      session_campaign,
+      session_source,
+      session_medium,
+      session_term,
+      session_gclid,
+      customer_group_name,
+
+      user_ltv_revenue,
+      user_ltv_currency,
+      user_campaign,
+      user_medium,
+      user_source,
+      user_referrer,
+      user_landing_page,
+      user_landing_page_type,
+      user_first_timestamp
+  )
+  SELECT 
+    
+    user_pseudo_id,
+    (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS ga_session_id, # not unique, but is per user_pseudo_id
+    CURRENT_TIMESTAMP(),
+    FORMAT_TIMESTAMP("%Y%m%d",SAFE.TIMESTAMP_MICROS(MIN(event_timestamp))) AS session_date,  
+    SAFE.TIMESTAMP_MICROS(MIN(event_timestamp)) AS session_start_timestamp, 
+    SAFE.TIMESTAMP_MICROS(MAX(event_timestamp)) AS session_end_timestamp, 
+    # session length
+    ANY_VALUE(user_id) AS user_id,
+
+    MAX(IF(event_name = 'first_visit',1,0)) AS session_first_visit,
+    MAX(IF(event_name = 'first_purchase',1,0)) AS session_first_purchase,
+
+    SUM(IF(event_name = 'page_view',1,0)) AS session_page_view_count,
+    SUM(IF(event_name = 'view_item_list',1,0)) AS session_view_item_list_count,
+    SUM(IF(event_name = 'view_item',1,0)) AS session_view_item_count,
+    SUM(IF(event_name = 'select_item',1,0)) AS session_select_item_count,
+    SUM(IF(event_name = 'add_to_cart',1,0)) AS session_add_to_cart_count,
+    SUM(IF(event_name = 'view_cart',1,0)) AS session_view_cart_count,
+    SUM(IF(event_name = 'begin_checkout',1,0)) AS session_begin_checkout_count,
+    SUM(IF(event_name = 'add_customer_info',1,0)) AS session_add_customer_info_count,
+    SUM(IF(event_name = 'add_shipping_info',1,0)) AS session_add_shipping_info_count, 
+    SUM(IF(event_name = 'add_billing_info',1,0)) AS session_add_billing_info_count,
+    #SUM(IF(event_name = 'add_payment_info',1,0)) AS session_add_payment_info_count, # same as purchase
+    SUM(IF(event_name = 'purchase',1,0)) AS session_purchase_count,
+    #SUM(IF(event_name = 'search',1,0)) AS session_search_count,
+
+    SUM(ecommerce.purchase_revenue) AS session_purchase_revenue, # is this only present on the purchase event?
+
+    ANY_VALUE(IF(event_name = 'purchase',(SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'currency'), NULL)) AS session_purchase_currency,
+
+    ANY_VALUE(device.category) AS session_device_category,
+    ANY_VALUE(device.web_info.browser) AS session_device_browser,
+    ANY_VALUE(device.operating_system) AS session_device_operating_system,
+
+    ANY_VALUE(geo.country) AS session_country,
+
+    # channel grouping (user/session) ????
+
+    ANY_VALUE(IF(event_name = 'session_start',(SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location'), NULL)) AS session_landing_page,
+    ANY_VALUE(IF(event_name = 'session_start',(SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_referrer'), NULL)) AS session_referrer,
+
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'campaign')) AS session_campaign,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'source')) AS session_source,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'medium')) AS session_medium,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'term')) AS session_term,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'gclid')) AS session_gclid,
+
+
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(user_properties) WHERE key = 'customer_group_name')) AS customer_group_name,
+    ANY_VALUE(user_ltv.revenue) AS	user_ltv_revenue,
+    ANY_VALUE(user_ltv.currency) AS user_ltv_currency,
+
+    ANY_VALUE(traffic_source.name)	AS user_campaign,
+    ANY_VALUE(traffic_source.medium)	AS user_medium,
+    ANY_VALUE(traffic_source.source)	AS user_source,
+
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(user_properties) WHERE key = 'referrer')) AS user_referrer,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(user_properties) WHERE key = 'landing_page')) AS user_landing_page,
+    ANY_VALUE((SELECT value.string_value FROM UNNEST(user_properties) WHERE key = 'landing_page_type')) AS user_landing_page_type,
+    SAFE.TIMESTAMP(ANY_VALUE((SELECT value.string_value FROM UNNEST(user_properties) WHERE key = 'first_datetime'))) AS user_first_timestamp,
+  FROM `${ProjectID}.${DatasetID}.events_*` 
+  WHERE event_name IN ('session_start', 'page_view', 'purchase', 'add_to_cart', 'begin_checkout', 'view_cart', 'view_item', 'view_item_list', 'first_visit', 'select_item', 'add_customer_info', 'add_shipping_info', 'add_billing_info', 'first_purchase')
+  AND (datetogather IS NULL OR _table_suffix BETWEEN FORMAT_DATE('%Y%m%d',datetogather) AND FORMAT_DATE('%Y%m%d',CURRENT_DATE()))
+  AND user_pseudo_id IS NOT NULL
+  GROUP BY 1, 2;
  
   # Billed Queries Log
 
